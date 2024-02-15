a0701008c0e385b0ba739a2dee88aa62a4b4bd73
stalinrangel <e.stalinrangel@gmail.com>
2024-02-15 17:04:51 -0400

    gvgg

--- bd.js
+++ bd.js
@@ -19,7 +19,8 @@ const knex=require('knex')({
         host:process.env.DB_HOST,
         user:process.env.DB_USER,
         password:process.env.DB_PASSWORD,
-        database:process.env.DB_NAME
+        database:process.env.DB_NAME,
+        port:process.env.DB_PORT
     },
     pool:{min:0,max:10}
 });
--- bd/migrations/20240201194640_add_user_roles.js
+++ bd/migrations/20240201194640_add_user_roles.js
@@ -3,9 +3,9 @@
  * @returns { Promise<void> }
  */
 exports.up = function(knex) {
-    return knex.schema.alterTable("legajos", (table) => {
-        table.integer('user_id',10).unsigned().references('id').inTable('users');
-    })
+    //return knex.schema.alterTable("legajos", (table) => {
+        //table.integer('user_id',10).unsigned().references('id').inTable('users');
+    //})
 };
 
 /**
