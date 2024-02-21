bc43071a69128801b3509064db8336d9ee292364
stalinrangel <e.stalinrangel@gmail.com>
2024-02-20 22:10:41 -0400

    ds

--- routes/ultramsgwebhook.js
+++ routes/ultramsgwebhook.js
@@ -6,22 +6,55 @@ var router = express.Router();
 const service=require('./../models/roles')
 
 
-router.get('/ultramsgwebhook', (req, res) => {
+router.get('/', (req, res) => {
     console.log(req.body) // print all response
-  
+    console.log(123)
     //messageFrom=req.body['data']['from'] // sender number
     //messageMsg=req.body['data']['body'] // Message text
     res.status(200).end()
   })
 
-router.post('/ultramsgwebhook', (req, res) => {
+router.post('/', (req, res) => {
     console.log(req.body) // print all response
 
     //messageFrom=req.body['data']['from'] // sender number
     //messageMsg=req.body['data']['body'] // Message text
     res.status(200).end()
 })
+const single=(req,res)=> {
+    console.log(req.body) // print all response
 
+    res.status(200).end()
+}
+const create=(req,res)=> {
+    const user= {name,designation} = req.body;
+    console.log(user)
+    return service
+    .create(user)
+    .then((response)=>res.json(response))
+    .catch((e)=>res.json({e}));
+}
+const update=(req,res)=> {
+    const user= {name,designation} = req.body;
+    console.log(user)
+    return service
+    .update({id:req.params.id},user)
+    .then((response)=>res.json(response))
+    .catch((e)=>res.json({e}));
+}
+
+const eliminate=(req,res)=> 
+    service.eliminate({id:req.params.id})
+    .then((response)=>res.json(response))
+    .catch((e)=>res.json({e}));
+
+
+router.get('/', single);
+
+router.post("/",create);
+
+module.exports = router;
+    
 
 
 module.exports = router;
