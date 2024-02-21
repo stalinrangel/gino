8964922c79c0381bda0e2916b0be197c30083119
stalinrangel <e.stalinrangel@gmail.com>
2024-02-20 22:06:01 -0400

    sd

--- app.js
+++ app.js
@@ -71,20 +71,6 @@ app.use('/images/app', express.static(__dirname + '/public/images/app'));
 const ultramsgwebhookRouter = require('./routes/ultramsgwebhook');
 app.use('/ultramsgwebhook', ultramsgwebhookRouter);
 
-app.post('/ultramsgwebhook', (req, res) => {
-  console.log(req.body) // print all response
-
-  //messageFrom=req.body['data']['from'] // sender number
-  //messageMsg=req.body['data']['body'] // Message text
-  res.status(200).end()
-})
-app.get('/ultramsgwebhook', (req, res) => {
-  console.log(req.body) // print all response
-
-  //messageFrom=req.body['data']['from'] // sender number
-  //messageMsg=req.body['data']['body'] // Message text
-  res.status(200).end()
-})
 
 
 // catch 404 and forward to error handler
