a5e0d2519e14f7c3a4f9657b514f75b8aa655103
stalinrangel <e.stalinrangel@gmail.com>
2024-02-21 19:11:57 -0400

    dsd

--- routes/ultramsgwebhook.js
+++ routes/ultramsgwebhook.js
@@ -148,19 +148,21 @@ function sendMessageToWhatsappCategorias( from, response) {
     
     return new Promise((resolve, reject) => {
     let lista=[];
+
     for (let i = 0; i < response.length; i++) {
         for (let j = 0; j < response[i].productos.length; j++) {
-            response[i].productos2[j].title='Proveedor: ' + response[i].productos[j].nombre;
-            response[i].productos2[j].description='ID: ' +response[i].productos[j].id + ' - ' +response[i].productos[j].descripcion;
+            response[i].productos[j].title='Proveedor: ' + response[i].productos[j].nombre;
+            response[i].productos[j].description='ID: ' +response[i].productos[j].id + ' - ' +response[i].productos[j].descripcion;
         }
         if (response[i].productos.length>0) {
             lista.push({
                 title: response[i].nombre,
-                rows: response[i].productos2
+                rows: response[i].productos
             });
         }
         
     }
+    
 
     const categ={
       "type": "list",
