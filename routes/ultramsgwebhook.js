70057fd7db2793b6379cda7f7f81cbc2970e37b4
stalinrangel <e.stalinrangel@gmail.com>
2024-02-21 00:00:20 -0400

    sd

--- routes/ultramsgwebhook.js
+++ routes/ultramsgwebhook.js
@@ -3,9 +3,10 @@ const cookieParser = require('cookie-parser');
 const { verify } = require('jsonwebtoken');
 require('dotenv').config();
 var router = express.Router();
-const service=require('./../models/roles')
 
-
+const dialogflow = require("./dialogflow");
+const axios = require('axios');
+const sessionIds = new Map();
 
 router.get('/', function(req, res, next) {
      // print all response
@@ -14,11 +15,11 @@ router.get('/', function(req, res, next) {
     console.log(modo); 
     
 
-    const verifyToken=modo['hub.verifyToken'];
+    const verifyToken=modo['hub.verify_token'];
     console.log(verifyToken); 
-    if (true) {
-        const challenge= modo['hub.challenge'];
-        console.log(challenge); 
+    if (verifyToken=='hola') {
+        
+        console.log('challenge'); 
         res.status(200).send(modo['hub.challenge']);
     }else{
         res.status(404).send('Acceso no autorizado');
@@ -31,8 +32,8 @@ router.post('/', function(req, res, next) {
      console.log(req.body.entry[0].changes[0].value.messages); 
     const modo=req.query;
     console.log(modo); 
-    
 
+    
     const verifyToken=modo['hub.verifyToken'];
     console.log(verifyToken); 
     if (true) {
@@ -44,6 +45,42 @@ router.post('/', function(req, res, next) {
     }
 });
 
+
+calldialogflow(){
+
+    let payload = dialogflow.sendToDialogFlow(message.body, session);
+    console.log(payload)
+    if (payload.action=="hola.action") {
+
+        enviarTexto(message);
+    }else if(payload.action=="frecuentes.action"){
+      
+        enviarTextoUrl(message);
+        enviarList(message);
+        enviarButtom(message);
+    }else if (payload.action=="categoria.info.action") {
+        subcategoria=payload.parameters.fields.categoriaName.stringValue;
+        productos= getProducts(payload.parameters.fields.categoriaName.stringValue);
+        let responses = payload.fulfillmentMessages;
+        for (const response of responses) {
+             sendMessageToWhatsappCategorias(client, message, productos);
+        }
+    }else if (payload.action=="proveedor.info.action") {
+        proveedor= getProveedor(payload.parameters.fields.number.numberValue);
+        console.log(proveedor)
+        let responses = payload.fulfillmentMessages;
+        for (const response of responses) {
+             sendMessageToWhatsappProveedor(client, message, proveedor);
+        }
+    }
+    else{    
+        let responses = payload.fulfillmentMessages;
+        for (const response of responses) {
+             sendMessageToWhatsapp(client, message, response);
+        }
+    }
+
+}
 
 
 module.exports = router;
