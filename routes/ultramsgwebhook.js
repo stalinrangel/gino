6f92f7354db87d86142dcd35139e5c5267b66ce5
stalinrangel <e.stalinrangel@gmail.com>
2024-02-21 00:17:53 -0400

    ds

--- routes/ultramsgwebhook.js
+++ routes/ultramsgwebhook.js
@@ -41,9 +41,10 @@ router.post('/', function(req, res, next) {
 });
 
 
-function calldialogflow(message,sesion){
-
-    let payload = dialogflow.sendToDialogFlow(message.body, session);
+function calldialogflow(text,from){
+    let session = sessionIds.get(from+'@c.us');
+    console.log(session)
+    let payload = dialogflow.sendToDialogFlow(text, session);
     console.log(payload)
     if (payload.action=="hola.action") {
 
@@ -75,6 +76,188 @@ function calldialogflow(message,sesion){
         }
     }
 
+}
+
+const token='EAAPDHrJXdvwBO9IzTuHU6JLQt0GgdXZCnHrji4meT0CN8xJgU64cNwJPorZAwFv3iMEYFyOEcF2luIOM5evIPH2KXJ0e1jZCjZBYt8tiGCsw5YKk4rOR4gGEggO0ynnxw4pRthqqZCL4Btidr08ZC84gQxx83Iok2CuqRXH06nIZAZBQZAhiuq13MQ72ofN8ZBAlum0ZCoZCazOonebHoK5z';
+
+function enviarTexto(mesanje){
+  const options={
+    method: 'POST',
+    url: 'https://graph.facebook.com/v18.0/216885471516427/messages',
+    headers: {
+      'Authorization': 'Bearer '+token,
+      'Content-Type': 'application/json'
+    },
+    data: { 
+      "messaging_product": "whatsapp", 
+      "to": "584127429278", 
+      "type": "template", 
+      "template": { 
+        "name": 
+        "hello_world", 
+        "language": {
+           "code": "en_US" 
+        } 
+      } 
+    }
+  };
+  console.log('-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------')
+  axios(options)
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+}
+
+function enviarTextoUrl(mesanje){
+  const options={
+    method: 'POST',
+    url: 'https://graph.facebook.com/v18.0/216885471516427/messages',
+    headers: {
+      'Authorization': 'Bearer '+token,
+      'Content-Type': 'application/json'
+    },
+    data: {
+      "messaging_product": "whatsapp",
+      "to": "584147428420",
+      "text": {
+          "preview_url": true,
+          "body": "Please visit https://youtu.be/hpltvTEiRrY to inspire your day!"
+      }
+    }
+  };
+  
+  axios(options)
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+}
+
+function enviarList(from,title,susbtitle,titleList,buttonText,lista){
+  const options={
+    method: 'POST',
+    url: 'https://graph.facebook.com/v18.0/216885471516427/messages',
+    headers: {
+      'Authorization': 'Bearer '+token,
+      'Content-Type': 'application/json'
+    },
+    data: {
+      "messaging_product": "whatsapp",
+      "recipient_type": "individual",
+      "to": "584147428420",
+      "type": "interactive",
+      "interactive": {
+          "type": "list",
+          "header": {
+              "type": "text",
+              "text": "<HEADER_TEXT>"
+          },
+          "body": {
+              "text": "<BODY_TEXT>"
+          },
+          "footer": {
+              "text": "<FOOTER_TEXT>"
+          },
+          "action": {
+              "button": "<BUTTON_TEXT>",
+              "sections": [
+                  {
+                      "title": "<LIST_SECTION_1_TITLE>",
+                      "rows": [
+                          {
+                              "id": "<LIST_SECTION_1_ROW_1_ID>",
+                              "title": "<SECTION_1_ROW_1_TITLE>",
+                              "description": "<SECTION_1_ROW_1_DESC>"
+                          },
+                          {
+                              "id": "<LIST_SECTION_1_ROW_2_ID>",
+                              "title": "<SECTION_1_ROW_2_TITLE>",
+                              "description": "<SECTION_1_ROW_2_DESC>"
+                          }
+                      ]
+                  },
+                  {
+                      "title": "<LIST_SECTION_2_TITLE>",
+                      "rows": [
+                          {
+                              "id": "<LIST_SECTION_2_ROW_1_ID>",
+                              "title": "<SECTION_2_ROW_1_TITLE>",
+                              "description": "<SECTION_2_ROW_1_DESC>"
+                          },
+                          {
+                              "id": "<LIST_SECTION_2_ROW_2_ID>",
+                              "title": "<SECTION_2_ROW_2_TITLE>",
+                              "description": "<SECTION_2_ROW_2_DESC>"
+                          }
+                      ]
+                  }
+              ]
+          }
+      }
+    }
+  };
+  
+  axios(options)
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+}
+
+function enviarButtom(mesanje){
+  const options={
+    method: 'POST',
+    url: 'https://graph.facebook.com/v18.0/216885471516427/messages',
+    headers: {
+      'Authorization': 'Bearer '+token,
+      'Content-Type': 'application/json'
+    },
+    data: {
+      "messaging_product": "whatsapp",
+      "recipient_type": "individual",
+      "to": "584147428420",
+      "type": "interactive",
+      "interactive": {
+          "type": "button",
+          "body": {
+              "text": "Seleccione una opcion:"
+          },
+          "action": {
+              "buttons": [
+                  {
+                      "type": "reply",
+                      "reply": {
+                          "id": "<UNIQUE_BUTTON_ID_1>",
+                          "title": "Buscar un proveedor"
+                      }
+                  },
+                  {
+                      "type": "reply",
+                      "reply": {
+                          "id": "<UNIQUE_BUTTON_ID_2>",
+                          "title": "Preguntas frecuentes"
+                      }
+                  }
+              ]
+          }
+      }
+    }
+  };
+  
+  axios(options)
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
 }
 
 
