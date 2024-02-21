d6d63325567b7fb8e2c26d84cde130485ffac100
stalinrangel <e.stalinrangel@gmail.com>
2024-02-20 22:53:57 -0400

    sd

--- routes/ultramsgwebhook.js
+++ routes/ultramsgwebhook.js
@@ -10,15 +10,14 @@ const service=require('./../models/roles')
 router.get('/', function(req, res, next) {
      // print all response
     const modo=req.query;
-    console.log(modo)
     console.log(modo['hub.mode']); 
     
 
-    const verifyToken=req.query.verifyToken;
+    const verifyToken=modo['hub.verifyToken'];
 
     if (verifyToken=='hola') {
         const challenge= req.query.challenge;
-        res.status(200).send(challenge);
+        res.status(200).send(modo['hub.challenge']);
     }else{
         res.status(404).send('Acceso no autorizado');
     }
