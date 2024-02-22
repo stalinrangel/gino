d22d5ad04f13690087b574f6d193330b317bacc4
stalinrangel <e.stalinrangel@gmail.com>
2024-02-21 20:12:31 -0400

    ds

--- routes/ultramsgwebhook.js
+++ routes/ultramsgwebhook.js
@@ -31,6 +31,7 @@ router.get('/', function(req, res, next) {
 
 router.post('/', function(req, res, next) {
      // print all response
+     res.status(200).send('Exito');
      console.log(req.body.entry[0].changes[0].value); 
 
 
