df765ef4afd81cd6f8ac34c6a739e9775e7a7060
stalinrangel <e.stalinrangel@gmail.com>
2024-03-12 15:39:33 -0400

    sd

---
+++ bd/migrations/20240312193448_add_relacion_ordenes_pedidos.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.alterTable("orders", (table) => {
+        table.integer('pedido_id');    
+    })
+    .alterTable("pedidos", (table) => {
+        table.integer('orden_id');
+    })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  
+};
